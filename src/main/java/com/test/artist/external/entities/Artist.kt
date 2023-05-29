010a15c60164ee73a2f93c7207d05ae6a34f8fb1
giulianogiannotti <71414041+giulianogiannotti@users.noreply.github.com>
2023-05-29 13:12:22 -0300

    Added New York Times Image

--- src/main/java/com/test/artist/external/entities/Artist.kt
+++ src/main/java/com/test/artist/external/entities/Artist.kt
@@ -1,10 +1,13 @@
 package com.test.artist.external.entities
 
+const val NY_TIMES_LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
+
 sealed class Artist {
     data class NYTimesArtist(
         var url: String? = null,
         var info: String? = "",
         var isLocallyStored: Boolean = false,
+        var logoImageUrl: String = NY_TIMES_LOGO_URL,
     ): Artist()
     object EmptyArtist: Artist()
 }
